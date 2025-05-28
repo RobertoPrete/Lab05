6907a5080c6dc29eda3d6a6c5ee895392662ed58
RobertoPrete <robertoprete2001@gmail.com>
2025-05-28 15:36:45 +0200

    sto provando a recuperare i dati dei corsi dal database

--- database/corso_DAO.py
+++ database/corso_DAO.py
@@ -1,3 +1,23 @@
 # Add whatever it is needed to interface with the DB Table corso
 
 from database.DB_connect import get_connection
+from model.corso import Corso
+
+
+class CorsoDAO:
+    @staticmethod
+    def get_Allcorsi():
+        cnx = get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        query = """SELECT * FROM corso c"""
+        cursor.execute(query)
+        res = []
+        # studenti = []
+        for row in cursor:
+            # codins = row["codins"]
+            # new_query = """select matricola from iscrizione i where codins=%s"""
+            # cursor.execute(new_query, codins)
+            # for riga in cursor:
+            # res.append(Corso(row["codins"], row["crediti"], row["nome"], row["pd"], ))
+        cnx.close()
+        return res
